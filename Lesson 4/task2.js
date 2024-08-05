8955b4a0f17e35624c87271be64530530a03bb20
bakovetskaaa <olyabakovetska@gmail.com>
2024-08-05 18:54:43 +0300

    Edited lesson 4

--- Lesson 4/task2.js
+++ Lesson 4/task2.js
@@ -1,13 +1,16 @@
-// погано працює, але працює з самим алгоритмом паліндрома
-
-let initualValue = "jJJ J";
+// variable - polindrom 
+let initualValue = "авссва";
+// initualization of value (with no current case and without gaps)
 let value = initualValue.replace(/\s+/g, "").toLowerCase();
+// variable lenght
 let len = value.length;
-
+// variable palindrome (true or false) for FOR
 let isPalindrom;
-
+// все в -1
+// а != -1-а
+// 0 != -1-0
 for (let i = 0; i < len / 2; i++) {
-    if (value[1] !== value[len - 1 - i]){
+    if (value[i] !== value[len - 1 - i]){
         isPalindrom = false;
         break;
     }
--- Lesson 4/task3.js
+++ Lesson 4/task3.js
@@ -1,22 +1,25 @@
 
-
+// Функція повертає діапазони / залежить від message
 function getValue(message) {
 
     while(true){ 
     let value = prompt(message);
-
+// валідне поки не: NaN, "", 0
     let isValid = !isNaN(value) && value !== "" && value !== null;
     if(isValid) return Number(value);
     }
 }
 
+// викликаємо двічі функцію для визначення діапазону
 let start = getValue("Введіть початок діапазону: ");
 let end = getValue("Введіть кінець діапазону: "); 
 
+// Цикл підраховує сумузначень діапазону
 if (end < start){
-    console.log("Кінець діапазону не може бути більшим за початок")
+    console.log("Кінець діапазону не може бути меншим за початок")
 } else {
     let sum = 0;
+// пробігаємо від початку до кінця збільшуючи на 1 значення і сумуючи їх
     for (let i = start; i <= end; i++) {
         sum = sum + i;
     }
