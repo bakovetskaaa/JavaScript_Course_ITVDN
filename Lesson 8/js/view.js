0489943209ab58eacfdbcf3500c94b9f8cea32d8
bakovetskaaa <olyabakovetska@gmail.com>
2024-09-04 14:28:27 +0300

    lesson 8

---
+++ Lesson 8/js/view.js
@@ -0,0 +1,28 @@
+const contactList = {
+    render(contacts, element){
+
+        contacts.array.forEach(contact => {
+            
+       
+            let section = document.createElement("#section");
+            section.classList.add("#contact");
+            section.insertAdjacentHTML("beforeend", '<h2>${contact.firstName} ${contact.lastName}<</h2>');
+            section.insertAdjacentHTML("beforeend", '<p>Phone: ${contact.phone}</p>');
+            section.insertAdjacentHTML("beforeend", '<p>Email: ${contact.email}</p>');
+            section.insertAdjacentHTML("beforeend", '<p>Description: ${contact.description}<p>');
+            element.appendChild(section);
+            
+    });
+
+    },
+
+    renderWithTemplate(contacts, element, template){
+        let html = "";
+        contacts.forEach(contact =>{
+            html += Mustache.render(template, contact);        
+        });
+        element.innerHTML = html;
+    }
+
+
+}
